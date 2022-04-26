a03392153a6c9e14308dafcbe0cf36000c1b1c7a
ronaldobarraca <104452474+ronaldobarraca@users.noreply.github.com>
2022-04-26 18:05:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,8 @@ streamlit.text('🥑🍞 Avocado Toast');
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
-
+#novo indice da lista
+my_fruit_list = my_fruit_list.set_index('Fruit');
 #multiselect
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index));
 
