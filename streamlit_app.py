78d671a87a23cd29a25fc59e9ad06d61c8914e42
ronaldobarraca <104452474+ronaldobarraca@users.noreply.github.com>
2022-04-26 17:54:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 
 import streamlit
 
-streamlit.title('My Mom\'s new Healthy Dinner');
+streamlit.title('My Parents new Healthy Dinner');
+
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 & Blueberry Oatmeal');
+streamlit.text('Kale, Spinach & Rocket Smoothie');
+streamlit.text('Hard-Boiled Free-Range Egg');
